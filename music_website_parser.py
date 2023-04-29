a0b4694794350c5110bee4166e680c400759aeeb
djanibekov <amir.akilbekovich@gmail.com>
2023-04-29 13:53:54 +0400

    [refactor]  dataset error resolution

--- music_website_parser.py
+++ music_website_parser.py
@@ -5,14 +5,18 @@ from bs4 import BeautifulSoup
 url = 'https://www.ocf.berkeley.edu/~acowen/music.html#'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-canvas = soup.find_all('div', class_='canvas')
+
+canvas_all = soup.find_all('div', attrs={'class': 'canvas', 'id':'allratings'})
+canvas_usa = soup.find_all('div', attrs={'class': 'canvas', 'id':'USA'})
+canvas_chn = soup.find_all('div', attrs={'class': 'canvas', 'id':'CHN'})
+
 dataframe1 = pd.DataFrame(columns=['id', 'ondblclick', 'onmouseover', 'class'])
 dataframe2 = pd.DataFrame(columns=['id', 'ondblclick', 'onmouseover', 'class'])
 dataframe3 = pd.DataFrame(columns=['id', 'ondblclick', 'onmouseover', 'class'])
 
 counter = 0
-for grid, dataframe, name in zip(canvas, [dataframe1, dataframe2, dataframe3], ['all', 'usa', 'china']):
-    genre_scanme = grid.find_all_next('div', class_='genre scanme')
+for grid, dataframe, name in zip([canvas_all[0], canvas_usa[0], canvas_chn[0]], [dataframe1, dataframe2, dataframe3], ['all', 'usa', 'china']):
+    genre_scanme = grid.find_all('div', attrs={'class': 'genre scanme'})
     data = {}
     for sample in genre_scanme:
         class_ = sample.get_text()
